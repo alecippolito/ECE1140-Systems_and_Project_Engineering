c3db6a39e1000d8fb4a5878673d06817dceebda0
Trent Howe <t-howe1@hotmail.com>
2022-04-04 15:40:18 -0400

    lsekfoiwefj

---
+++ TrainModel/Block.h
@@ -0,0 +1,56 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <string>
+
+class Block {
+
+    public:
+            bool occupied;
+            int blockNumber;
+            char section;
+            double blockLength;
+            double blockGrade;
+            int speedLimitKmHr;
+            double speedLimitMSec;
+            double elevation;
+            double cumElevation;
+            int timeToTravelBlock;
+            Block *previousBlock, *nextBlock1, *nextBlock2;    //nextBlock2 only if track has 2 choices (Switch is active)
+            std::string to, from;
+
+
+
+            Block(int num){
+                blockNumber = num;
+            }
+
+            bool isOccupied();
+            void setBlockNumber(int);
+            void setSection(char);
+            void setBlockLength(double);
+            void setBlockGrade(double);
+            void setSpeedLimitKmHr(int);
+            void setSpeedLimitMSec(double);
+            void setElevation(double);
+            void setCumElevation(double);
+            void setTimeToTravelBlock(int);
+            void setPreviousBlock(Block);
+            void setNextBlock1(Block);
+            void setNextBlock2(Block);
+            void setTo(std::string);
+            void setFrom(std::string);
+
+            double getBlockLength();
+
+
+
+
+
+
+
+
+
+
+};
+#endif // BLOCK_H
