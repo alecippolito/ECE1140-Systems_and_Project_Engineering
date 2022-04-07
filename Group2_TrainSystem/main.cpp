95a24974be0cbaa424223fed42bac5121f7bd914
JackBCollins <83829381+JackBCollins@users.noreply.github.com>
2022-04-07 15:18:35 -0400

    Fixed Main.cpp

--- Group2_TrainSystem/main.cpp
+++ Group2_TrainSystem/main.cpp
@@ -1,12 +1,15 @@
-#include "mainwindow.h"
-#include "PLC.h"
+#include "system_centraltimer_connector.h"
 
 #include <QApplication>
+
+//the main file where the Central Timer UI is called
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    System_CentralTimer_Connector w;
     w.show();
     return a.exec();
+
+    //yerrrr
 }
